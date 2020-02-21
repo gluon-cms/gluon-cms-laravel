859073981f5ecc566a4e7f2c167149aed0dfb256
Nicolas Brignol <tools@octet-studio.com>
2020-02-21 11:33:02 +0100

    WIP : query builder rehydradation

--- app/Http/Controllers/GluonApiController.php
+++ app/Http/Controllers/GluonApiController.php
@@ -19,11 +19,11 @@ class GluonApiController extends Controller
         
         $query->select([
             'gluon_entity.id as entity_id', 
-            'text__title.value as text__title', 
-            'text__content.value as text__content',
+            'text__title.value as entity__text__title', 
+            'text__content.value as entity__text__content',
 
-            'related__associated.related_entity_id as associated__id',
-            'related__associated__text__title.value as associated__text__title',
+            'related__associated.related_entity_id as related__associated__id',
+            'related__associated__text__title.value as related__associated__text__title',
         ]);
 
         $query->leftJoin('gluon_param_text as text__title', function ($join) {
@@ -51,14 +51,64 @@ class GluonApiController extends Controller
         });
 
 
-        $query->where('gluon_entity.id', $id);
+        //$query->where('gluon_entity.id', $id);
+        $query->orderBy('entity_id');
+        //$query->orderBy('entity__text__title');
 
-        $entities = $query->get();
-        dd($entities);
+        $lines = $query->get();
 
+
+        $baseEntitiesById = [];
+        $relatedEntitiesById = [];
+
+        $entities = [];
+
+        function getOrCreateEntity($id, &$entitiesById, &$entities = []) {
+
+            $entity = null;
+
+            if(! isset($entitiesById[ $id ])) {
+                $entitiesById[ $id ] = new \stdClass(); //GluonEntityResult
+                $entity = $entitiesById[ $id ];
+
+                $entities[] = $entity;
+            } else {
+                $entity = $entitiesById[ $id ];
+            }
+
+            $entity->id = $id;
+            return $entity;
+        };
+
+
+        foreach ($lines as $line) {
+            $entity = getOrCreateEntity($line->entity_id, $baseEntitiesById, $entities);
+
+            //(re)populate entity
+
+            //loop on entity__* properties
+            $entity->text__title = $line->entity__text__title;
+            $entity->text__content = $line->entity__text__content;
+
+            //loop on related__*
+            if (! isset($entity->related__associated)){
+                $entity->related__associated = [];
+            }
+
+            if ($line->related__associated__id) {
+                $relatedAssociatedEntity = getOrCreateEntity($line->related__associated__id, $relatedEntitiesById);
+
+                $entity->related__associated[] = $relatedAssociatedEntity;
+
+                //loop on related__associated__* properties
+                $relatedAssociatedEntity->text__title = $line->related__associated__text__title;
+            }
+        }
+
+        
         return response()->json([
-            'name' => 'Abigail',
-            'state' => 'CA'
+            'lines' => $lines,
+            'entities' => $entities
         ]);
     }
 
